01f4e949cb1e4447e59e4486edc21bbddd558d02
foz <foz>
2023-08-04 23:53:32 +0100

    Update model link

--- get_file.sh
+++ get_file.sh
@@ -12,6 +12,6 @@ sudo apt-get update
 sudo apt-get install -y git-lfs
 
 git lfs install
-git clone https://huggingface.co/JFoz/test_nvs
+git clone https://huggingface.co/JFoz/
 
 pip install accelerate clean-fid clip-anytorch einops jsonmerge kornia Pillow resize-right scikit-image scipy torchdiffeq torchsde torchvision tqdm wandb protobuf==3.20 wandb einops fastai protobuf==3.20 omegaconf hydra-core
--- make_videos.sh
+++ make_videos.sh
@@ -4,10 +4,10 @@ python sample_views.py --transfer=../genvs-unofficial/genvs.pt  --progress --pre
 python sample_views.py --transfer=../genvs-unofficial/genvs.pt  --progress --prefix uc --unconditional --stochastic --n 1
 python sample_views.py --transfer=../genvs-unofficial/genvs.pt  --progress --prefix uc_det --unconditional --n 1
 #
-for i in {0..1}; do ffmpeg -r 10 -i ${p}/cars-sample-1-00000${i}-%d.png ${p}/cars-sample-${i}.mp4; done
-for i in {0..1}; do ffmpeg -r 10 -i ${p}/cars_det-sample-1-00000${i}-%d.png ${p}/cars_det-sample-${i}.mp4; done
-for i in {0..1}; do fmpeg -i ${p}/cars-step-1-00000${i}-%d.png ${p}/cars-${i}.mp4; done
-for i in {0..1}; do fmpeg -i ${p}/cars_det-step-1-00000${i}-%d.png ${p}/cars_det-${i}.mp4; done
+for i in {0..1}; do ffmpeg -r 10 -i ${p}/cars-sample-1-00000${i}-%d.png ${p}/cars-${i}.mp4; done
+for i in {0..1}; do ffmpeg -r 10 -i ${p}/cars_det-sample-1-00000${i}-%d.png ${p}/cars_det-${i}.mp4; done
+for i in {0..1}; do ffmpeg -i ${p}/cars-step-1-00000${i}-%d.png ${p}/cars-sample-${i}.mp4; done
+for i in {0..1}; do ffmpeg -i ${p}/cars_det-step-1-00000${i}-%d.png ${p}/cars_det-sample-${i}.mp4; done
 
 ffmpeg  -i ${p}/uc-step-1-000000-%d.png ${p}/uc.mp4
 ffmpeg  -i ${p}/uc_det-step-1-000000-%d.png ${p}/uc_det.mp4
